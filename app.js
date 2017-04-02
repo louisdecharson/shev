d5e96397f4884ee6ba64737303ad75991915d1a9
Louis de Charson <ltdc2121@hotmail.fr>
2017-04-03 01:08:18 +0200

    add timezone

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var express = require('express'),
     favicon = require('serve-favicon'),
     mongodb = require("mongodb"),
     ical = require('ical-generator'),
-    shortid = require('shortid');
+    shortid = require('shortid'),
+    MapboxClient = require('mapbox'),
+    https = require('https'),
+    moment = require('moment-timezone');
 
-
+require('dotenv').config({path:path.join(__dirname,'/.env')});
 
 var app = express();
 const port = process.env.PORT || 8080; 
@@ -94,30 +97,77 @@ app.get('/',function(req,res){
 app.post('/',function(req,res){
     var newEv = req.body;
     newEv.idEv = shortid.generate();
-    db.collection('events').insertOne(newEv, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, "Failed to create new event.");
-        } else {
-            res.redirect('/view/'+newEv.idEv);
-        }
-    });
+    if (newEv.loc !== undefined) {
+        var mapboxClient = new MapboxClient(process.env.MAPBOX_ACCESSTOKEN);
+        var options = {query: newEv.loc,limit: 1};
+        mapboxClient.geocodeForward(newEv.loc, options, function(err, response) {
+            var lng = response.features[0].geometry.coordinates[0],
+                lat = response.features[0].geometry.coordinates[1];
+            var myPath = '/maps/api/timezone/json?location='+ lat + ',' + lng + '&timestamp=1331161200&key=' + process.env.GMAP_APIKEY;
+            var options = {
+                hostname: 'maps.googleapis.com',
+                port: 443,
+                path: myPath,
+                headers: {'connection': 'keep-alive'}
+            };
+            https.get(options, function(result) {
+                if (result.statusCode >=200 && result.statusCode < 400) {
+                    var json = '';
+                    result.on('data', function(chunk) {
+                        json += chunk;
+                    });
+                    result.on('end',function() {
+                        var timezone = JSON.parse(json)['timeZoneId'];
+                        newEv.userDate = newEv.startDate;
+                        console.log(newEv.startDate);
+                        var startDate = moment.tz(newEv.startDate,"MM/DD/YYYY h:mm A",timezone).format();
+                        var endDate = moment.tz(newEv.endDate,"MM/DD/YYYY h:mm A",timezone).format();
+                        newEv.startDate = startDate;
+                        newEv.endDate = endDate;
+                        newEv.timezone = timezone;
+                        console.log(newEv);
+                        db.collection('events').insertOne(newEv, function(err, doc) {
+                            if (err) {
+                                handleError(res, err.message, "Failed to create new event.");
+                            } else {
+                                res.redirect('/view/'+newEv.idEv);
+                            }
+                        });
+                    });
+                } else {
+                }
+            });
+        });
+    } else {
+        db.collection('events').insertOne(newEv, function(err, doc) {
+            newEv.notes = newEv.notes + ' WARNING : TIME IS GMT';
+            if (err) {
+                handleError(res, err.message, "Failed to create new event.");
+            } else {
+                res.redirect('/view/'+newEv.idEv);
+            }
+        });
+    }
 });
 
 app.get('/view/:id',function(req,res){
     var idEv =  req.params.id;
-    db.collection('events').findOne({'idEv': idEv},{'name':1,'loc':1,'startDate':1, 'endDate':1,'notes':1,'alarm':1,'idEv':1}, function(err,doc) {
+    db.collection('events').findOne({'idEv': idEv},{'name':1,'loc':1,'startDate':1, 'endDate':1,'notes':1,'alarm':1,'idEv':1, 'timezone':1}, function(err,doc) {
         if (err) {
             handleError(res,err.message,"Failed to get post");
         } else if (doc !== null) {
-            doc.url = "http://sharev.herokuapp.com/ev/" + doc.idEv;
+            doc.url = "http://localhost:8080/ev/" + doc.idEv;
             if (doc.notes === null){doc.notes = "";}
-            var startDate = new Date(doc.startDate);
-            var endDate = new Date(doc.endDate);
-            if (startDate.getDate() === endDate.getDate() && startDate.getFullYear() === endDate.getFullYear() && startDate.getMonth() === endDate.getMonth()) {
-                doc.day = startDate.getDate().toString() + '/' + (startDate.getMonth()+1).toString() + '/' + startDate.getFullYear().toString();
-                doc.startTime = startDate.getHours().toString() + 'h' + (startDate.getMinutes()<10?'0':'').toString() + startDate.getMinutes().toString();
-                doc.endTime = endDate.getHours().toString() + 'h' + (endDate.getMinutes()<10?'0':'').toString() + endDate.getMinutes();
-            } 
+            var startDate = moment.tz(doc.startDate,doc.timezone);
+            var endDate = moment.tz(doc.endDate,doc.timezone);
+            if (startDate.date() === endDate.date() && startDate.year() === endDate.year() && startDate.month() === endDate.month()) {
+                doc.day = startDate.format('D') + '/' + startDate.format('M') + '/' + startDate.format('YYYY');
+                doc.startTime = startDate.format('hh') + 'h' + startDate.format('mm') + ' ' + startDate.format('A');
+                doc.endTime = endDate.format('hh') + 'h' + endDate.format('mm') + ' ' + endDate.format('A') + ' (GMT' + endDate.format('Z') + ')' ;
+            } else {
+                doc.startTime = startDate.format('D') + '/' + startDate.format('M') + '/' + startDate.format('YYYY') + ' at ' + startDate.format('hh') + 'h' + startDate.format('mm') + ' ' + startDate.format('A');
+                 doc.endTime = endDate.format('D') + '/' + endDate.format('M') + '/' + endDate.format('YYYY') + ' at ' + endDate.format('hh') + 'h' + endDate.format('mm') + ' ' + endDate.format('A') + ' (GMT' + endDate.format('Z') + ')' ;
+            }
             res.render('view_ev',{ev:doc});
         } else {
             handleError(res,"Failed to get event","Failed to get event");
